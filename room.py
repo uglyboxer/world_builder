acae1c953f746bb93ab767ada0ca3f3ece79cc26
Cole Howard <cole@subtlegears.com>
2015-09-13 19:33:24 -0700

    Clean up comments

--- room.py
+++ room.py
@@ -1,4 +1,5 @@
-from random import choice, randint, randrange
+from random import choice, randint
+
 
 class room:
 
@@ -7,41 +8,39 @@ class room:
         self.possible_exits = possible_exits
         self.exits = randint(1, len(self.possible_exits))
         self.actual_exits = [self.possible_exits.pop(self.possible_exits.index(choice(self.possible_exits)))
-                             for i in range(self.exits)] #need to pop from list
+                             for i in range(self.exits)]
 
-        #Adversaries
-        #Items (and their location, called from another class)
-        #Depth in dungeon
-        #"Layer" in story (which of the Five Room Dungeon, is this?)
-
-
-
-# Perhaps these end up in different modules?
+        # Adversaries
+        # Items (and their location, called from another class)
+        # Depth in dungeon
+        # "Layer" in story (which of the Five Room Dungeon, is this?)
+        # Perhaps these end up in different modules?
 
 class item:
 
     def __init__(self):
-        #item_name
-        #place in the room
-        #moveble?
+        # item_name
+        # place in the room
+        # moveble?
         pass
 
 class npc:
 
     def __init__(self):
-        #species
-        #gender
-        #size
-        #speed
-        #weapon
-        #defense
-        #attack
-        #things it may say
+        # species
+        # gender
+        # size
+        # speed
+        # weapon
+        # defense
+        # attack
+        # things it may say
         pass
 
 
 def check_neighbors(dungeon, dungeon_room, entered_from):
-
+    # Check surrounding 10 neighbors, and ignore if 0
+    # Act accordingly in designing the room if neighbor exists and doors or walls line up
     possible_exits = ['u', 'd', 'w']
 
     return possible_exits
@@ -54,14 +53,11 @@ def print_dungeon(level, dungeon):
             print(dungeon[level][i][j], " ", end="")
         print("\n")
 
+# Random start of dungeon (the descent) and develop as it goes.
 
-
-# Create a list of lists for dungeon with 0 placeholders to start.
-# Random start of dungeon (the descent) and develop as it goes.  Check surrounding 10 neighbors, and ignore if 0
-# Act accordingly in designing the room if neighbor exists and doors or walls line up
 
 def main():
-    dungeon = [[[0 if (
+    dungeon = [[[0 if (         # Creates a basic framework for room objects to land on
         (i != 0) and
         (i != 19) and
         (j != 0) and
@@ -71,10 +67,6 @@ def main():
     start_location = randint(1, 18), randint(1,18)
     possible_exits = ['u', 'd', 'w', 'e', 'n']
     dungeon[0][start_location[1]][start_location[0]] = room(possible_exits)
-    # print(dungeon[0][start_location[1]][start_location[0]].actual_exits)
-    # ('n', 'ne', 'e', 'se', 's', 'sw', 'w', 'nw', 'u', 'd')
-    print_dungeon(0, dungeon)
-
-
+    print_dungeon(0, dungeon)       # Just print first level of dungeon for now
 
 main()
